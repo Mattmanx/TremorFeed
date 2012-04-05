697e84c7551e7f6a2a02be7dcd8a1bd44850caa9
Matt Mehalso <mmehalso@captechconsulting.com>
2012-04-05 13:03:37 -0400

    Initial commit.

---
+++ src/com/captech/mehalso/tremorfeed/TremorListActivity.java
@@ -0,0 +1,13 @@
+package com.captech.mehalso.tremorfeed;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TremorListActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
